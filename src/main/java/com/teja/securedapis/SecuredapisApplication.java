8da589522773431714f731f337ceb333a0a47610
PrudviTeja99 <prudvitejabandam987@gmail.com>
2025-04-11 15:49:06 +0530

    initial commit

---
+++ src/main/java/com/teja/securedapis/SecuredapisApplication.java
@@ -0,0 +1,13 @@
+package com.teja.securedapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredapisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teja/securedapis/SecuredapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teja.securedapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
